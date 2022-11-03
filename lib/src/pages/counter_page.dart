6d0733d4a9c2cd0ee37f2650c0575aec11e3bc48
Adriano Anadinho <adrianoanadinho@gmail.com>
2022-11-03 15:38:42 -0300

    final refactor

--- lib/src/pages/counter_page.dart
+++ lib/src/pages/counter_page.dart
@@ -28,33 +28,65 @@ class _CounterViewState extends State<CounterView> {
       appBar: AppBar(
         title: const Text('Counter App'),
       ),
-      body: Center(
-        child: BlocBuilder<CounterCubit, int>(
-          bloc: _cubit,
-          builder: (context, state) {
-            return Text(
-              '$state',
-              style: textTheme.headline2,
-            );
-          },
+      body: BodyWidget(cubit: _cubit, textTheme: textTheme),
+      floatingActionButton: ButtonWidget(cubit: _cubit),
+    );
+  }
+}
+
+class BodyWidget extends StatelessWidget {
+  const BodyWidget({
+    Key? key,
+    required CounterCubit cubit,
+    required this.textTheme,
+  })  : _cubit = cubit,
+        super(key: key);
+
+  final CounterCubit _cubit;
+  final TextTheme textTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: BlocBuilder<CounterCubit, int>(
+        bloc: _cubit,
+        builder: (context, state) {
+          return Text(
+            '$state',
+            style: textTheme.headline2,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({
+    Key? key,
+    required CounterCubit cubit,
+  })  : _cubit = cubit,
+        super(key: key);
+
+  final CounterCubit _cubit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        FloatingActionButton(
+          key: UniqueKey(),
+          child: const Icon(Icons.add),
+          onPressed: () => _cubit.increment(),
         ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            key: UniqueKey(),
-            child: const Icon(Icons.add),
-            onPressed: () => _cubit.increment(),
-          ),
-          FloatingActionButton(
-            key: UniqueKey(),
-            child: const Icon(Icons.remove),
-            onPressed: () => _cubit.decrement(),
-          )
-        ],
-      ),
+        FloatingActionButton(
+          key: UniqueKey(),
+          child: const Icon(Icons.remove),
+          onPressed: () => _cubit.decrement(),
+        )
+      ],
     );
   }
 }
